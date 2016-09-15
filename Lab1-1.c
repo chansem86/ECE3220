ede5cff9f7382fa2d700aa13d56dabf5222f0cc8
Chanse Meyer <Chanse Meyer>
2016-09-15 10:40:06 -0500

    first time on git...

---
+++ Lab1-1.c
@@ -0,0 +1,25 @@
+/*
+============================================================================
+ Name        : Lab1-1.c
+ Author      : Chanse Meyer
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Area of Circle
+ ============================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define PI 3.14
+
+int main(void) {
+	float radius = 0;
+	float area;
+	printf("Enter the radius of your circle:"); /*Ask user for radius */
+	fflush(stdout);
+	scanf("%f",&radius);
+	area = PI*radius*radius; /* Calculates area */
+	printf ("The area of the circle is %.2f", area);
+	return EXIT_SUCCESS;
+}
